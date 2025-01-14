56a5e04b8b5202ed0bcc4236f5df06ea522a617d
Sketchy-C <willy254dabs@gmail.com>
2025-01-13 23:22:45 -0800

    new

--- my_app/urls.py
+++ my_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.homepage,name='home'),
-    path('profile/',views.profile,name='profile'),
+    path('', views.homepage, name='home'),
+    path('profile/', views.profile, name='profile'),
     path('logout/', views.logout, name='logout'),
+    path('<str:image_id>/', views.image_detail, name='image_detail'),
 ]
